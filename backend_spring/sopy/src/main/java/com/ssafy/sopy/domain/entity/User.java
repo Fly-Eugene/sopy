6e243fe057393d6d58de27087b7ff5a2408ccf49
김종혁 <alb7979s@naver.com>
2021-09-08 15:25:58 +0900

    feat: jwt 토큰 인증

--- backend_spring/sopy/src/main/java/com/ssafy/sopy/domain/entity/User.java
+++ backend_spring/sopy/src/main/java/com/ssafy/sopy/domain/entity/User.java
@@ -1,11 +1,55 @@
 package com.ssafy.sopy.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
+@Getter
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 30, nullable = false)
+    private String username;
+
+    @Column
+    private Integer age;
+
+    @Column(length = 30)
+    private String department;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
+    public User() {
+    }
+
+    @Builder
+    public User(Long id, String email, String password, String username, Integer age, String department, Set<Authority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.age = age;
+        this.department = department;
+        this.authorities = authorities;
+    }
 }
--- backend_spring/sopy/src/main/java/com/ssafy/sopy/dto/UserDto.java
+++ backend_spring/sopy/src/main/java/com/ssafy/sopy/dto/UserDto.java
@@ -1,4 +1,9 @@
 package com.ssafy.sopy.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UserDto {
+    private String email;
+    private String password;
 }
