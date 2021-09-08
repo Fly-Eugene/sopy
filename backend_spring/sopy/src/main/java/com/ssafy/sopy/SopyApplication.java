0038dd9798fc283f8539a6eedef875e8a451be35
김종혁 <alb7979s@naver.com>
2021-09-08 11:14:30 +0900

    build: spring init

---
+++ backend_spring/sopy/src/main/java/com/ssafy/sopy/SopyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopyApplication.class, args);
+    }
+
+}
