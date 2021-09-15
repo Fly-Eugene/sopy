07e43809b25eb72f58962ce1ff932996f1e564d4
이유진 <eujjin@naver.com>
2021-09-15 17:33:58 +0900

    feat: OCR 모델 Django 적용

--- backend_django/AI/web/views.py
+++ backend_django/AI/web/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 import requests
-
+import os
+import shlex
+import subprocess
 
 # Create your views here.
-@api_view(['POST'])
+
+# text 파일 저장해서, 경로 보내기 !!
+
+
+@api_view(['GET'])
 def book_ocr(request):
-    txt_file = request.FILES['file']
+    # txt_file = request.FILES['file']
+    txt_file = './OCR_img/'
 
-    # txt_file = request.FILES
+    command_line = f"python demo.py \
+    --Transformation None \
+    --FeatureExtraction VGG \
+    --SequenceModeling BiLSTM \
+    --Prediction CTC \
+    --image_folder demo_image/ \
+    --input_channel 1 \
+    --output_channel 256 \
+    --hidden_size 256 \
+    --saved_model ./saved_models/None-VGG-BiLSTM-CTC-Seed1111/best_accuracy.pth"
 
+    args = shlex.split(command_line)
+
+    print('==================================================================')
+    print(args)
+    pwd = os.getcwd()
+    os.chdir('./deep-text-recognition-benchmark')
+    p = subprocess.Popen(args)
+    # os.chdir('./web')
+    # p = subprocess.Popen("python ttest.py")
+    print('==================================================================')
+
+    return Response('123456789')
