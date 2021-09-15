fedd6385941c1d99dc855781bb4ca9a5e2de70f1
hyeyemin <wlalsdns@naver.com>
2021-09-15 10:10:06 +0900

    feat: step01

---
+++ frontend/src/administor/component/Step01.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Step01 = () => {
+    return (
+        <div>
+            <h2>책 표지를 선택해주세요</h2>
+            
+            <button></button>
+        </div>
+    );
+}
+export default Step01;
