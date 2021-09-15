f491a63e713a34acb57ea95091d1b12e412af8d6
hyeyemin <wlalsdns@naver.com>
2021-09-15 10:19:56 +0900

    feat:bookcreate_step01

--- frontend/src/administor/component/Step01.js
+++ frontend/src/administor/component/Step01.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
+const Btn = styled.button`
+  margin-left: auto;
+  word-break: break-all;
+  font-size: 16px;
+  line-height: 1;
+  font-weight: 400;
+  color: #ffdac7;
+  border-radius: 15px; 
+`;
+
 const Step01 = () => {
     return (
         <div>
             <h2>책 표지를 선택해주세요</h2>
             
-            <button></button>
+            <Btn>사진 선택</Btn>
         </div>
     );
 }
