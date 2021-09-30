16aa8e0376ffa020d26e5c510c32baab1639aa39
hyeyemin <wlalsdns@naver.com>
2021-09-30 20:47:12 +0900

    feat:좋아하는 책 목록

--- frontend/src/user/component/UserLikeBook.js
+++ frontend/src/user/component/UserLikeBook.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './UserLikeBook.modules.scss'
 import Book from '../../common/component/Book'
 import underImgSrc from '../../img/book_under.png';
 import bookCover from '../../img/book-cover.jpg'
 import nextBtn from '../../img/nextBtn.png'
 import prevBtn from '../../img/prevBtn.png'
+import { useDispatch } from 'react-redux';
+import { getLikes } from '../../store/actions/bookActions';
+import { useHistory } from 'react-router';
+import { Grid } from '@material-ui/core';
 
 export default function UserLikeBook() {
+  const [booklist, setBooklist] = useState([]);
+  useEffect(() => {
+    getBookList();
+  },[])
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const getBookList = e => {
+    dispatch(getLikes())
+    .then((res) => {
+      console.log(res.payload.data.book)
+      setBooklist(res.payload.data.book)
+    })
+    .catch((err) => console.log(err));
+  }
   const prev = () => {
     return 
   }
   const next = () => {
     return
   }
+  const moveDetail = (params, e) =>{
+    history.push({
+        pathname: "/book",
+        state: {book: params}
+    });
+}
   return (
     <div className="like-book-container">
       <div className="like-book-inner">
@@ -21,9 +45,13 @@ export default function UserLikeBook() {
           <span>1 / 20</span>
         </div>
         <div className="book-container">
-          <Book underImgSrc={underImgSrc} bookCover={bookCover}/>
-          <Book underImgSrc={underImgSrc} bookCover={bookCover}/>
-          <Book underImgSrc={underImgSrc} bookCover={bookCover}/>
+          {
+            booklist.map((book) => {
+              <Grid item xs={4} onClick={(e) => {moveDetail(book, e)}}>
+                <Book underImgSrc={underImgSrc} bookCover={book.bookImage.path + book.bookImage.imageName}/>      
+              </Grid>
+            })
+          }
         </div>
       </div>
       <div className="like-botton-direction">
